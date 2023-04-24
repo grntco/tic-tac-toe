efb7e8262ee5efa7548160639f5ecea0535567f2
Grant Collins <grantcollins03@gmail.com>
2023-04-24 19:21:12 -0400

    app js notes

---
+++ app.js
@@ -0,0 +1,10 @@
+const gameboard = (() => {
+    const arr = [];
+    // DOM elements, connecting things, pushing onto array
+    return arr;
+})
+
+const player = function() {
+    // player stuff
+    // return the player stuff you need
+}
