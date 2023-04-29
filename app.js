4f1e5af1858bc23a6295e89cd0476336daba7fdd
Grant Collins <grantcollins03@gmail.com>
2023-04-29 11:12:33 -0400

    Robots and aliens I guess

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ const createPlayer = (name, mark) => {
     return { name, mark, points, placeMark }
 };
 
-const player1 = createPlayer('Grant', '<i class="fa-solid fa-x"></i>');
-const player2 = createPlayer('Hal', '<i class="fa-solid fa-o"></i>');
+const player1 = createPlayer('Grant', '🤖');
+const player2 = createPlayer('Hal', '👽');
 
 const game = (() => {
 
@@ -95,3 +95,7 @@ const game = (() => {
 
     return { switchTurns, getActivePlayer, checkPlay }
 })();
+
+// const displayController = () => {
+
+// }
